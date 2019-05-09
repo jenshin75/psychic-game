57e72c9d3684d29fef269c2d9e690d1ca577cb52
jenshin75 <49443598+jenshin75@users.noreply.github.com>
2019-05-09 11:19:42 -0500

    updated 05/09/19 11:19am

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,10 @@
-// Creates an array that lists out all of the options (Rock, Paper, or Scissors).
 var computerChoice = ["r", "p", "s"];
 
 // Creating variables to hold the number of wins, losses, and ties. They start at 0.
 var wins = 0;
 var losses = 0;
 var guessCnt = 3;
-var wrongLetter = "";
+var wrongGuessLetter = "";
 var userChoice = "";
 
 // Create variables that hold references to the places in the HTML where we want to display things.
@@ -15,7 +14,7 @@ var computerChoiceText = document.getElementById("computerChoice-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessCntText = document.getElementById("guessCnt-text");
-var wrongLetterText = document.getElementById("wrongLetter-text");
+var wrongGuessLetterText = document.getElementById("wrongGuessLetter-text");
 
 function resetGame() {
     directions = "";
@@ -24,41 +23,44 @@ function resetGame() {
     // wins = 0;
     // losses = 0;
     guessCnt = 3;
-    wrongLetter = "";
+    wrongGuessLetter = "";
 }
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
 
-// Determines which key was pressed.
-var userGuess = event.key.toLowerCase ();
+    // Determines which key was pressed.
+     userGuess = event.key.toLowerCase();
 
-var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 
-// This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-if  (userGuess === computerGuess)
-    {wins++;
-    guessCnt;}
-else
-    {guessCnt--;
-    // document.write("You won! Let's play again.")
-     losses++;}
-    
-if (guessCnt === 0)
-    {losses++;
-    // document.write("You lost! Let's try again.");
-    resetGame();}
+    // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+    if (userGuess === computerGuess) {
+           // document.write("You won! Let's play again.")
+        wins++;
+         console.log("Wins: " + wins);
+         resetGame();
+        }
+    else {
+        guessCnt--;
+        wrongGuessLetter = wrongGuessLetter + userGuess + ",";
+        wrongGuessLetterText.textContent = wrongGuessLetter;
+    }
 
-directionsText.textContent = "";
+    if (guessCnt === 0)
+    { 
+        losses++; 
+        console.log("Losses: " + losses);
+        resetGame();
+    }
+   
+    directionsText.textContent = "";
 
-// Display the user and computer guesses, and wins/losses/ties.
-userChoiceText.textContent = "You chose: " + userGuess;
-computerChoiceText.textContent = "The computer chose: " + computerGuess;
-winsText.textContent = "Wins: (# of times you guessed the letter correctly) " + wins;
-lossesText.textContent = "Losses: (# of times you failed to guess the letter correctly) " + losses;
-guessCntText.textContent = "Guesses Left: " + guessCnt;
-wrongLetterText.textContent = "Your Guesses So Far: " + wrongLetter;
-
+     // Display the user and computer guesses, and wins/losses/ties.
+     userChoiceText.textContent = "You chose: " + userGuess;
+     computerChoiceText.textContent = "The computer chose: " + computerGuess;
+     winsText.textContent = "Wins: (# of times you guessed the letter correctly) " + wins;
+     lossesText.textContent = "Losses: (# of times you failed to guess the letter correctly) " + losses;
+     guessCntText.textContent = "Guesses Left: " + guessCnt;
+     wrongGuessLetterText.textContent = "Your Guesses So Far: " + wrongGuessLetter;
 }
-
-
